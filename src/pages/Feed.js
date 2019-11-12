48a7bff842d66a53bf21bbe42392504671ec4546
Wígny Almeida <wignybora@gmail.com>
2019-11-11 21:53:24 -0400

    Ajuste pro trabalho de amanhã

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -37,7 +37,7 @@ class Feed extends Component {
                 <img src={more} alt="Mais" />
               </header>
 
-              <img src={`http://localhost:8080/files/${post.image}`} alt="" />
+              <img src={`https://backend-likeinsta.herokuapp.com/files/${post.image}`} alt="" />
 
               <footer>
                 <div className="actions">
